8a5f447f6285a95de8c01c8a1ca1c75d42235c41
dr-ko <skoirala@bgc-jena.mpg.de>
2025-05-21 06:57:21 +0200

    fix lue script to use the config file for opti

--- tutorials/ai4pex_2025/test_LUE.jl
+++ tutorials/ai4pex_2025/test_LUE.jl
@@ -36,7 +36,7 @@ replace_info = Dict("experiment.basics.time.date_begin" => begin_year * "-01-01"
     "experiment.model_output.format" => "nc",
     "experiment.model_output.save_single_file" => true,
     "experiment.exe_rules.parallelization" => parallelization_lib,
-    "optimization.algorithm" => "opti_algorithms/Optim_LBFGS.json",
+    "optimization.algorithm_optimization" => "CMAEvolutionStrategy_CMAES.json",
     "optimization.observations.default_observation.data_path" => path_observation);
 
 info = getExperimentInfo(experiment_json; replace_info=replace_info); 
