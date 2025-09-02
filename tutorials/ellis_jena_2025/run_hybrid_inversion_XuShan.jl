5ef09d9f9dad94e05a2a726c96859fec08f0dd0e
dr-ko <skoirala@bgc-jena.mpg.de>
2025-09-02 10:21:47 +0200

    add mpcdf path for data

--- tutorials/ellis_jena_2025/run_hybrid_inversion_XuShan.jl
+++ tutorials/ellis_jena_2025/run_hybrid_inversion_XuShan.jl
@@ -20,7 +20,7 @@ end
 # data to be used can be found here: https://nextcloud.bgc-jena.mpg.de/s/w2mbH59W4nF3Tcd
 # organizing the paths of data sources and outputs for this experiment
 path_input_dir      = getSindbadDataDepot(; env_data_depot_var="SINDBAD_DATA_DEPOT", 
-                    local_data_depot=joinpath(@__DIR__,"..","data","ellis_jena_2025")); # for convenience, the data file is set within the SINDBAD-Tutorials path; this needs to be changed otherwise.
+                    local_data_depot=joinpath("/home/jovyan/data/ellis_jena_2025")); # for convenience, the data file is set within the SINDBAD-Tutorials path; this needs to be changed otherwise.
 path_input          = joinpath("$(path_input_dir)","FLUXNET_v2023_12_1D_REPLACED_Noise003_v1.zarr"); # zarr data source containing all the data for site level runs
 path_observation    = path_input; # observations (synthetic or otherwise) are included in the same file
 path_covariates     = joinpath("$(path_input_dir)","CovariatesFLUXNET.zarr"); # zarr data source containing all the covariates
